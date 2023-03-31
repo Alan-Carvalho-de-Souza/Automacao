2ff08e8a973d441ee255d0b4008fb2130e8b3850
Alan-Carvalho-de-Souza <107443541+Alan-Carvalho-de-Souza@users.noreply.github.com>
2023-03-31 18:08:10 -0300

    Update orçamento.py

--- orçamento.py
+++ orçamento.py
@@ -4,3 +4,8 @@ Testando conhecimentos de automação de tarefas
 Objetivo criar uma automação, para enviar uma planilha com os dados de orçamento pessoal via e-mail
 """
 
+import pandas as pd
+import pyautogui as pa
+import time
+import pyperclip
+
