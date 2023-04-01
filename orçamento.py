a510579282912aac94dc283d87dc5cd0064911b4
Alan-Carvalho-de-Souza <107443541+Alan-Carvalho-de-Souza@users.noreply.github.com>
2023-04-01 17:10:37 -0300

    Update orçamento.py

--- orçamento.py
+++ orçamento.py
@@ -9,3 +9,12 @@ import pyautogui as pa
 import time
 import pyperclip
 
+#time.sleep(4)
+#print(pa.position())
+
+pa.click(x=556, y=748)
+time.sleep(3)
+pa.write('gmail.com')
+pa.press('enter')
+time.sleep(2)
+pa.click(x=75, y=207)
