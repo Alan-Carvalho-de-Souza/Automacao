2853ce05ca9b67913248324ed817fb21746b1b1f
Alan-Carvalho-de-Souza <107443541+Alan-Carvalho-de-Souza@users.noreply.github.com>
2023-04-03 20:54:37 -0300

    Update autweb.py

--- autweb.py
+++ autweb.py
@@ -12,3 +12,7 @@ navegador.get('https://www.google.com.br/')
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação dolar")
 
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+
+cotacao_dolar = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+
+print(cotacao_dolar)
