1b44af8ac4a329c685e6128123ef9269dfdab902
Alan-Carvalho-de-Souza <107443541+Alan-Carvalho-de-Souza@users.noreply.github.com>
2023-04-04 18:02:48 -0300

    Update autweb.py

--- autweb.py
+++ autweb.py
@@ -4,29 +4,35 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 navegador = webdriver.Chrome()
+navegador.implicitly_wait(4)
 
 # Passo 1: Pegar cotação do dolar
 
 navegador.get('https://www.google.com.br/')
-navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação dolar")
+navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("gmail")
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
-cotacao_dolar = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
-print(cotacao_dolar)
+navegador.find_element('xpath', '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div[1]/a/h3').click()
+navegador.find_element('xpath', '/html/body/header/div/div/div/a[2]').click()
+
+
+#cotacao_dolar = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+#print(cotacao_dolar)
 
 # Passo 2: Pegar cotação do euro
-
+"""
 navegador.get('https://www.google.com.br/')
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação euro")
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
 cotacao_euro = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
 print(cotacao_euro)
-
+"""
 # Passo 3: Pegar cotação do bitcoin
+"""
+navegador.get('https://www.google.com.br/')
+navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação bitcoin")
+navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+cotacao_bitcoin = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('pclqee')
 
-#navegador.get('https://www.google.com.br/')
-#navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação bitcoin")
-#navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
-#cotacao_bitcoin = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('pclqee')
-
-#print(cotacao_bitcoin)
+print(cotacao_bitcoin)
 print('A cotação do dólar é: ', cotacao_dolar)
+"""
