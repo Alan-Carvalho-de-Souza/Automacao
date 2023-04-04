3f3fcf5bf2a3d06e2dea088dc1cfff8e76dec2f9
Alan-Carvalho-de-Souza <107443541+Alan-Carvalho-de-Souza@users.noreply.github.com>
2023-04-04 19:36:58 -0300

    Update autweb.py

--- autweb.py
+++ autweb.py
@@ -1,4 +1,4 @@
-# automação web com selenium
+# automação web com selenium, pyautogui e pandas
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -15,7 +15,7 @@ navegador.get('https://www.google.com.br/')
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação dolar")
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
 cotacao_dolar = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
-
+valor_dolar = float(cotacao_dolar)
 
 # Passo 2: Pegar cotação do euro
 
@@ -23,7 +23,7 @@ navegador.get('https://www.google.com.br/')
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação euro")
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
 cotacao_euro = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
-
+valor_euro = float(cotacao_euro)
 
 # Passo 3: Pegar cotação do bitcoin
 
@@ -31,7 +31,17 @@ navegador.get('https://www.google.com.br/')
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação bitcoin")
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
 cotacao_bitcoin = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('pclqee')
+valor_bitcoin = float(cotacao_bitcoin)
 
 # Passo 4: Ordenar numa tabela os valores coletados
+moedas = {
+    'moeda': ['Bitcoin', 'Dólar', 'Euro'],
+    'valores2': [valor_bitcoin, valor_dolar, valor_euro]
+}
+dataframe = pd.DataFrame(moedas)
+print(dataframe)
+
 # Passo 5: Gerar um arquivo Excel
+
+dataframe.to_excel('C:\Automacao\moedas.xlsx')
 # Passo 6: Enviar o arquivo via e-mail
