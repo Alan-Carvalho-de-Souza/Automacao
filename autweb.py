fe1d4b104e16e4a17832f55be1da45b30b9cb3d1
Alan-Carvalho-de-Souza <107443541+Alan-Carvalho-de-Souza@users.noreply.github.com>
2023-04-04 19:57:06 -0300

    Update autweb.py

--- autweb.py
+++ autweb.py
@@ -30,18 +30,18 @@ valor_euro = float(cotacao_euro)
 navegador.get('https://www.google.com.br/')
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação bitcoin")
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
-cotacao_bitcoin = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('pclqee')
+cotacao_bitcoin = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
 valor_bitcoin = float(cotacao_bitcoin)
 
 # Passo 4: Ordenar numa tabela os valores coletados
 moedas = {
     'moeda': ['Bitcoin', 'Dólar', 'Euro'],
-    'valores2': [valor_bitcoin, valor_dolar, valor_euro]
+    'valores': [valor_bitcoin, valor_dolar, valor_euro]
 }
 dataframe = pd.DataFrame(moedas)
 print(dataframe)
 
 # Passo 5: Gerar um arquivo Excel
 
-dataframe.to_excel('C:\Automacao\moedas.xlsx')
+dataframe.to_excel('moedas.xlsx', index=False)
 # Passo 6: Enviar o arquivo via e-mail
