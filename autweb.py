e0c3427efe85041338dbb3505cf8608631161b51
Alan-Carvalho-de-Souza <107443541+Alan-Carvalho-de-Souza@users.noreply.github.com>
2023-04-04 19:09:52 -0300

    Update autweb.py

--- autweb.py
+++ autweb.py
@@ -2,6 +2,9 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import pyautogui as pa
+
 
 navegador = webdriver.Chrome()
 
@@ -9,36 +12,26 @@ navegador = webdriver.Chrome()
 # Passo 1: Pegar cotação do dolar
 
 navegador.get('https://www.google.com.br/')
-navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("gmail")
+navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação dolar")
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
-navegador.find_element('xpath', '//*[@id="rso"]/div[1]/div/div/div/div/div/div/div[1]/a/h3').click()
-navegador.find_element('xpath', '/html/body/header/div/div/div/a[2]').click()
-navegador.find_element('xpath', '//*[@id="identifierId"]').send_keys('alan.carvalhosouza96@gmail.com')
-navegador.find_element('xpath', '//*[@id="identifierId"]').send_keys(Keys.ENTER)
-navegador.implicitly_wait(10)
-navegador.find_element('xpath', '//*[@id="password"]/div[1]/div/div[1]/input').send_keys('#AlanCS1996#')
-navegador.find_element('xpath', '//*[@id="password"]/div[1]/div/div[1]/input').send_keys(Keys.ENTER)
-navegador.implicitly_wait(10)
-navegador.find_element('xpath', '/html/body/div[7]/div[3]/div/div[2]/div[1]/div[1]/div/div').click()
+cotacao_dolar = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
 
-#cotacao_dolar = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
-#print(cotacao_dolar)
 
 # Passo 2: Pegar cotação do euro
-"""
+
 navegador.get('https://www.google.com.br/')
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação euro")
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
 cotacao_euro = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
-print(cotacao_euro)
-"""
+
+
 # Passo 3: Pegar cotação do bitcoin
-"""
+
 navegador.get('https://www.google.com.br/')
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação bitcoin")
 navegador.find_element('xpath', '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
 cotacao_bitcoin = navegador.find_element('xpath', '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('pclqee')
 
-print(cotacao_bitcoin)
-print('A cotação do dólar é: ', cotacao_dolar)
-"""
+# Passo 4: Ordenar numa tabela os valores coletados
+# Passo 5: Gerar um arquivo Excel
+# Passo 6: Enviar o arquivo via e-mail
